41915aeb0e27812b435f7e68e3cab97b7f51c409
Alejo <alejocaj9@gmail.com>
2025-09-05 23:58:27 -0300

    Configurar para deploy: TypeScript + Prisma + Railway2.0

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { AuthController } from '../controllers/auth.controller';
+import { AuthController } from '../controllers/auth.controllers';
 import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = Router();
